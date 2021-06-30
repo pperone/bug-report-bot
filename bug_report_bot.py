15210164b7cdd914bad1dcdf38caecc5955b46f8
pperone <pperone@gmail.com>
2021-06-30 14:10:25 -0700

    Back to legacy bot

--- bug_report_bot.py
+++ bug_report_bot.py
@@ -1,16 +1,24 @@
 import os
-# Use the package we installed
+import logging
+
 from slack_bolt import App
 
-
 # Initializes your app with your bot token and signing secret
+logging.basicConfig(level=logging.DEBUG)
+
 app = App(
     token=os.environ.get("BRBOT_TOKEN"),
     signing_secret=os.environ.get("BRBOT_SECRET")
 )
 
-# Add functionality here
-# @app.event("app_home_opened") etc
+# Listens to incoming messages that contain "hello"
+# To learn available listener arguments,
+# visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    logger
+    say(f"Hey there <@{message['user']}>!")
 
 # Start your app
 if __name__ == "__main__":
