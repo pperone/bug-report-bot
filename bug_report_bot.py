697108cb2f6a838483e77504fc5ac89ed912f3cf
pperone <pperone@gmail.com>
2021-07-01 06:53:02 -0700

    Passing on bot

--- bug_report_bot.py
+++ bug_report_bot.py
@@ -1,6 +1,4 @@
 import os
-import time
-import re
 
 from slack_sdk.rtm_v2 import RTMClient
 
@@ -20,16 +18,15 @@ RTM_READ_DELAY = 1
 # Processes the message
 @rtm.on("message")
 def handle(client: RTMClient, event: dict):
+    # debug = "{'type': 'message', 'subtype': 'bot_message', 'text': '6/30/2021 17:18:27\n\n*Reporter:*\ntest\n<mailto:test@test.com|test@test.com>\n\n*Area:*\nBossSite\n\n*Summary of Issue:*\ntest\n\n*How to Reproduce:*\n\n\n*URL:*\n\n\n*User ID(s):*\n\n\n*Transaction ID(s):*\n\n\n*Vehicle ID(s):*\n\n\n*Screenshots:*\n\n\n\n\n\n*Severity?*\nPERFORMANCE ISSUE - Application is taking too long to perform\n', 'suppress_notification': False, 'username': 'BugBot', 'icons': {'emoji': ':bug:', 'image_64': 'https://a.slack-edge.com/production-standard-emoji-assets/13.0/apple-large/1f41b.png'}, 'bot_id': 'B0VCNJ3QR', 'team': 'T02FPTL8D', 'bot_profile': {'id': 'B0VCNJ3QR', 'deleted': False, 'name': 'Zapier', 'updated': 1587492924, 'app_id': 'A024R9PQM', 'icons': {'image_36': 'https://slack-files2.s3-us-west-2.amazonaws.com/avatars/2017-06-20/200850512066_2d5e268a3b71c87f969c_36.png', 'image_48': 'https://slack-files2.s3-us-west-2.amazonaws.com/avatars/2017-06-20/200850512066_2d5e268a3b71c87f969c_48.png', 'image_72': 'https://slack-files2.s3-us-west-2.amazonaws.com/avatars/2017-06-20/200850512066_2d5e268a3b71c87f969c_72.png'}, 'team_id': 'T02FPTL8D'}, 'source_team': 'T02FPTL8D', 'user_team': 'T02FPTL8D', 'channel': 'C0SLYBWQZ', 'event_ts': '1625091664.320700', 'ts': '1625091664.320700'}"
+    print(event)
+
     if event["type"] == "message":
-        if "attachments" in event:
-            print(event)
-            client.web_client.chat_postMessage(
-                channel = debug_channel,
-                text = ":burger: Nothingburger"
-            )
+        if event["subtype"] == "bot_message":
+            pass
         else:
             client.web_client.chat_postMessage(
-                channel = debug_channel,
+                channel = CHANNEL,
                 text = ":warning: *Reminder:* if you'd like to report a bug, please fill out *<https://goo.gl/forms/yLWoLKdMDHdfLmkf2|this form>* and add as much information as possible. Thank you!"
             )
 
