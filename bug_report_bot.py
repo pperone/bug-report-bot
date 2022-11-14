6274534bf32e883993540bccc1382b0ab939bd0b
pedro perone <perone@pedros-MacBook-Pro-2.local>
2022-11-14 10:46:09 -0800

    Selecting channel

--- bug_report_bot.py
+++ bug_report_bot.py
@@ -20,21 +20,21 @@ RTM_READ_DELAY = 1
 def handle(client: RTMClient, event: dict):
     mention = "<@" + event["user"] + ">"
 
-    if event["type"] == "message":
-        if "subtype" in event:
-            pass
-        elif "thread_ts" in event:
-            pass
-        elif "message" in event:
-            if "thread_ts" in event["message"]:
+    if event["channel"] == CHANNEL
+        if event["type"] == "message":
+            if "subtype" in event:
                 pass
-        else:
-            print(event)
-            client.web_client.chat_postMessage(
-                channel = CHANNEL,
-                thread_ts = event["ts"],
-                text = "%s: if you'd like to report a bug, please fill out the form at *<https://goo.gl/forms/yLWoLKdMDHdfLmkf2|https://goo.gl/forms/yLWoLKdMDHdfLmkf2>* and add as much information as possible. Thank you!" %(mention)
-            )
+            elif "thread_ts" in event:
+                pass
+            elif "message" in event:
+                if "thread_ts" in event["message"]:
+                    pass
+            else:
+                client.web_client.chat_postMessage(
+                    channel = CHANNEL,
+                    thread_ts = event["ts"],
+                    text = "%s: if you'd like to report a bug, please fill out the form at *<https://goo.gl/forms/yLWoLKdMDHdfLmkf2|https://goo.gl/forms/yLWoLKdMDHdfLmkf2>* and add as much information as possible. Thank you!" %(mention)
+                )
 
 
 # Initializer
